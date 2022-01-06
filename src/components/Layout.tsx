26ba934b7c9724ccd81fd46c50bebe29da9bfbd0
logantai24 <logantai24@gmail.com>
2022-01-06 12:35:47 +0800

    add default style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 export const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen w-full">
       {children}
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center py-2">
+    <div className="mx-auto flex flex-col items-center justify-center py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
