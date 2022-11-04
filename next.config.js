89c3db43621fc5d9d8e4157365e48260484bf2ec
logantai24 <logantai24@gmail.com>
2022-11-04 07:29:21 +0000

    remove: react-strict-mode

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  reactStrictMode: true,
   pwa: {
     dest: "public",
     register: true,
