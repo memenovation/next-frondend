ba501648e0e06b60fec472e4ff32bc289029e83d
logantai24 <logantai24@gmail.com>
2022-11-04 07:33:39 +0000

    update: next-config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
-const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
-  },
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
 });
+
+module.exports = withPWA({});
