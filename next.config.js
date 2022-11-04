7d9197ff9b3708e51ac746cf9579100a59affd1f
logantai24 <logantai24@gmail.com>
2022-11-04 07:30:54 +0000

    update: next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
     dest: "public",
     register: true,
