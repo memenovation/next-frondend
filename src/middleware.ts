c749bc5d369686f27647c68d4d4495e851d90d53
Logan Tai <logantai24@gmail.com>
2023-02-05 17:08:04 +0000

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/((?!auth|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!auth|api|_next/static|_next/image|favicon.ico).*)"],
 };
 
 export default withAuth({});
